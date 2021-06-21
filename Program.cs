072afee787f8284ef224aefc07daff9186a507ae
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-06-22 00:39:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo s1 = new StudentInfo();
+            s1.SetId("$100");
+            s1.SetName("manasa");
+            s1.SetMarks(90);
+
+            string idValue = s1.GetId();
+            string nameValue = s1.GetName();
+            int marksValue = s1.GetMarks();
+            System.Console.WriteLine(idValue);
+            System.Console.WriteLine(nameValue);
+            System.Console.WriteLine(marksValue);
+
+        }
+    }
+}
---
+++ StudentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class StudentInfo
+    {
+        private string id;
+        private string name;
+        private int marks;
+
+        public string GetId() { return this.id; }
+        public string GetName() { return this.name; }
+        public int GetMarks() { return this.marks; }
+
+        public void SetId(string value) { this.id = value; }
+        public void SetName(string value) { this.name = value; }
+        public void SetMarks(int value) { this.marks = value; }
+    }
+}
